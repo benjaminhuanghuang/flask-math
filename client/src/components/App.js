169f3ceac515a1783c1fec2acd9467f168c94468
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-24 16:36:00 -0700

    client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,26 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 //
-import Header from './Header';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import SurveyNew from './Surveys/SurveyNew';
 
 import * as actions from '../actions';
 
 class App extends Component {
   componentDidMount(){
-    this.props.fetchUser();
+
   }
 
   render() {
     return (
        <BrowserRouter>
         <div className="container">
-          <Header/>
-          <Route exact path="/" component={Landing}/>
-          <Route exact path="/surveys" component={Dashboard}/>
-          <Route exact path="/surveys/new" component={SurveyNew}/>
+          <div>App</div>
         </div>
        </BrowserRouter>
     );
